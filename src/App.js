7efb3ad7d472b250febc9fab4fe2b718dd91b8a2
MaicolGARCIA1 <115556588+MaicolGARCIA1@users.noreply.github.com>
2024-04-17 11:28:48 -0500

    Interaccion de Calificaciones

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Interactions from './Components/Interactions/Interactions';
+
+function App() {
+  return (
+    <>
+    <Interactions></Interactions>
+    </>
+  );
+}
+
+export default App;
