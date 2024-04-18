e9ad7d94dffa9e007fa985231fbb77132089a6fa
MaicolGARCIA1 <115556588+MaicolGARCIA1@users.noreply.github.com>
2024-04-18 12:06:30 -0500

    Cambios totalmente en todo el proyecto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Interactions from './Components/Interactions/Interactions';
+// import Advertisement from './Components/Advertisement/Advertisement';
 
 function App() {
   return (
     <>
     <Interactions></Interactions>
+    {/* <Advertisement></Advertisement> */}
     </>
   );
 }
