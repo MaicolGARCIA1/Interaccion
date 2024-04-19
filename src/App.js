7794b73b25e2fbf08d02a9b79971de0a63ba877b
MaicolGARCIA1 <115556588+MaicolGARCIA1@users.noreply.github.com>
2024-04-19 16:23:57 -0500

    Cambios Totales

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Interactions from './Components/Interactions/Interactions';
-// import Advertisement from './Components/Advertisement/Advertisement';
 
 function App() {
   return (
     <>
     <Interactions></Interactions>
-    {/* <Advertisement></Advertisement> */}
     </>
   );
 }
